d1d7b021efdc75825a64d4e8803c1507bff8f662
Marina A. Usova <oppabang@mail.ru>
2025-03-05 14:05:37 +0300

    add start structure of project

---
+++ SportComplex/Date/date.h
@@ -0,0 +1,12 @@
+// Copyright 2025 Marina Usova
+
+#ifndef DATE_H_
+#define DATE_H_
+
+class Date {
+    int _year;
+    int _month;
+    int _day;
+};
+
+#endif  // DATE_H_
---
+++ SportComplex/TestApplication/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "date.h"
+
+int main() {
+    Date date_1;
+    return 0;
+}
